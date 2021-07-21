a0e875e98ab2737c76601870f7689ea5fc02fa9e
Emetselch1984 <banzai.nippon2030@gmail.com>
2021-07-22 07:09:36 +0900

    #3 [add] CalenderDetails.vue

--- frontend/src/components/CalenderDetails.vue
+++ frontend/src/components/CalenderDetails.vue
@@ -1,22 +1,19 @@
 <template>
   <div>
-    <h1>Calender</h1>
-    <ul>
-      <li v-for="event in events" :key="event.id">
-        {{ event.name }}
-      </li>
-    </ul>
-    <button type="submit" @click="fetchEvents">fetchEvents</button>
-    <CalenderDetails/>
+    <h1>CalenderDetails</h1>
+    <div v-for="event in events" :key="event.id">
+      <h2>{{ event.name }}</h2>
+      <p>start: {{ event.start }}</p>
+      <p>end: {{ event.end }}</p>
+      <p>description: {{ event.description }}</p>
+    </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
-import CalenderDetails from "@/components/CalenderDetails";
 export default {
-  name: "Calender",
-  components: {CalenderDetails},
+  name: "CalenderDetails",
   data: () => ({
     events: [],
   }),
