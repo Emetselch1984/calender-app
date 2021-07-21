410fef14ac91519bab2bc29517af61ec5bf5a4f1
Emetselch1984 <banzai.nippon2030@gmail.com>
2021-07-22 06:50:35 +0900

    #3 [add] Calender.vue

---
+++ frontend/src/components/Calender.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>Calender</h1>
+    <p>events</p>
+    <p>{{events}}</p>
+    <button type="submit" @click="fetchEvents">fetchEvents</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: "Calender",
+  data: () => ({
+    events: [],
+  }),
+  methods: {
+    fetchEvents() {
+      axios
+          .get('http://localhost:3000/events')
+          .then(response => {
+            this.events = response.data;
+          })
+          .catch(error => {
+            console.error(error);
+          });
+    }
+  }
+};
+</script>
